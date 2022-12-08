d4f2fd8073982d0099bce1d71d85b842cbaa76a2
Slava Khripushin <slava.cpa@yandex.ru>
2022-12-08 14:01:04 +0700

    init

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+/* eslint-disable operator-linebreak */
+/* eslint-disable no-inner-declarations */
+
+// const viewportFix = (width) => {
+//   const meta = $('meta[name="viewport"]');
+//   meta.attr('content', 'user-scalable=no, width=' + (screen.width <= width ? width : 'device-width'));
+// };
+
+// viewportFix(375);
+
+Fancybox.bind('[data-fancybox]', {
+  dragToClose: false,
+  autoFocus: false,
+  placeFocusBack: false,
+});
+
+const maskPhone = () => {
+  const maskedElements = [];
+  const el = document.querySelectorAll('.masked');
+  if (el.length > 0) {
+    const mask = {
+      mask: '+7 (000) 000-00-00',
+    };
+    el.forEach((item) => {
+      maskedElements.push(new IMask(item, mask));
+    });
+  }
+  $('.masked').click(function () {
+    if ($(this).val() == '') $(this).val('+7 ');
+  });
+};
+
+maskPhone();
